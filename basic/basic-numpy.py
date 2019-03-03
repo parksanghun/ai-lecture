8dee363fd1a16d9abc9f43ff691abe9b802efe42
sanghun <sanghun1984@gmail.com>
2019-03-03 12:58:10 +0900

    init p

--- basic/basic-numpy.py
+++ basic/basic-numpy.py
@@ -3,3 +3,10 @@ import numpy as np
 ar=np.array([1,2,3])
 print("타입", type(ar), "Data Type:", ar.dtype)
 
+ar=ar*2
+arlist=[]
+for a in ar:
+    arlist.append(a*2)
+print(arlist)
+arlist=arlist*2
+print("ar:", ar, "arlist:", arlist)
---
+++ basic/basic.py
@@ -0,0 +1,12 @@
+a=1
+print(a, type(a))
+b=1.0
+print(b, type(b))
+c="파이썬"
+print(c, type(c))
+d=[1,2,1.0,"인공지능", [5,6]]
+print(type(d))
+
+# 집합 {} 중복 허용하지 않음
+g={1,2,2,3.0,'deep learning'}
+print(g, type(g))
